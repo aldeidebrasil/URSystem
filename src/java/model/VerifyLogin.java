c3b8668b1c202ce00659ae49adeace54133fbb9b
aldeidebrasil <aldeidebrasil@gmail.com>
2015-02-17 16:05:45 -0500

    Revert "Insert, Update, Delete Student -  OK"
    This reverts commit 27820f34cd46690068640989ef9db5dcb599c691.

---
+++ src/java/model/VerifyLogin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Aldeide Brasil
+ */
+public class VerifyLogin {
+     private static PreparedStatement pstmt = null;
+    private static ResultSet rs = null;
+    
+    
+}
